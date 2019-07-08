73323663c47168ae52ec31e7deb5c34fbfae0480
Nackenmassage <oliverfeske@hotmail.com>
2019-07-08 15:19:11 +0200

    CharacterMovement

---
+++ Assets/_Scripts/_Character/_Movement/CharacterMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 5f;
+
+    private void Start()
+    {
+        // Start the game without Cursor
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    private void Update()
+    {
+        // sort it by Input, Movement, Jump,(maybe Input for actions)
+
+        float translation = Input.GetAxis("Vertical") * moveSpeed;
+        float strafe = Input.GetAxis("Horizontal") * moveSpeed;
+        translation *= Time.deltaTime;
+        strafe *= Time.deltaTime;
+
+        transform.Translate(strafe, 0f, translation);
+    }
+
+}
---
+++ Assets/_Scripts/_Character/_Movement/CharacterRotation.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterRotation : MonoBehaviour
+{
+    Vector2 mouseLook;
+    Vector2 smooth;
+
+    [SerializeField] private float sensitivity = 1f;
+    [SerializeField] private float smoothing = 2f;
+
+    GameObject character;
+
+    private void Start()
+    {
+        character = this.transform.parent.gameObject;
+    }
+
+    private void Update()
+    {
+        var mouseDelta = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
+
+        mouseDelta = Vector2.Scale(mouseDelta, new Vector2(sensitivity * smoothing, sensitivity * smoothing));
+        smooth.x = Mathf.Lerp(smooth.x, mouseDelta.x, 1f / smoothing);
+        smooth.y = Mathf.Lerp(smooth.y, mouseDelta.y, 1f / smoothing);
+        mouseLook += smooth;
+
+        transform.localRotation = Quaternion.AngleAxis(-mouseLook.y, Vector3.right);
+        character.transform.localRotation = Quaternion.AngleAxis(mouseLook.x, character.transform.up);
+    }
+}
