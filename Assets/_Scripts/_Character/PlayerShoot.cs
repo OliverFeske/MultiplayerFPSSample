b16efc40dfcc1b53f0929c9b760cb124a9f5d6f6
Nackenmassage <oliverfeske@hotmail.com>
2019-07-10 11:13:00 +0200

    Folder sorting

---
+++ Assets/_Scripts/_Character/PlayerShoot.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerShoot : NetworkBehaviour
+{
+    public PlayerWeapon weapon;
+
+    [SerializeField] private Camera cam;
+
+    private void Start()
+    {
+        if(cam == null)
+        {
+            Debug.LogError("PlayerShoot: No Camera referred!");
+            this.enabled = false;
+        }
+    }
+}
