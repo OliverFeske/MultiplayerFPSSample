1953999a8007aaef4feb3506d72acce5ef96ad49
Nackenmassage <oliverfeske@hotmail.com>
2019-08-01 18:37:00 +0200

    Lightmap / UserDatabase / Login and Logout

---
+++ Assets/_Scripts/_Manager/UserAccount_Lobby.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace MultiFPS
+{
+	public class UserAccount_Lobby : MonoBehaviour
+	{
+		public Text usernameText;
+
+		void Start()
+		{
+			if (UserAccountManager.IsLoggedIn)
+			{
+				usernameText.text = UserAccountManager.PlayerUsername;
+			}
+
+		}
+
+		public void LogOut()
+		{
+			if (UserAccountManager.IsLoggedIn)
+			{
+				UserAccountManager.instance.LoggedOut();
+			}
+		}
+	}
+}
